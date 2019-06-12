cd25da882be10aece07492e28f60bd85682a8c75
lizdoyle <lizdoyle1121@gmail.com>
2019-06-11 22:58:10 -0400

    working still

--- Article/Article.js
+++ Article/Article.js
@@ -3,17 +3,24 @@
 class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
-    this.domElement = 
+    this.domElement = domElement;
     // create a reference to the ".expandButton" class. 
-    this.expandButton;
+    this.expandButton = domElement.querySelector(".expand-button");
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
+    this.expandButton.textContent = 'expand';
     // Set a click handler on the expandButton reference, calling the expandArticle method.
     
+    this.expandButton.addEventListener('click', this.expandArticle());
+
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
+    this.expandButton.classList.toggle(".article-open");
+    this.expandButton.classList.toggle(".close");
+    this.expandButton.classList.toggle(".article");
+
+
 
   }
 }
@@ -30,7 +37,7 @@ let articles = document.querySelectorAll(".articles");
 let article = document.querySelectorAll(".article");
 let openArticle = document.querySelectorAll(".article-open");
 let closeArticle = document.querySelectorAll(".close");
-let articleDate = document.querySelectorAll(".date");
+
 
 
 articles.forEach(item => {
--- Menu/Menu.js
+++ Menu/Menu.js
@@ -2,7 +2,8 @@
 const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence. 
   
-  // menu.classList.(".menu--open")
+  menu.classList.toggle(".menu--open");
+  menu.classList.toggle(".menu-button");
 }
 
 // Start Here: Create a reference to the ".menu" class
@@ -10,6 +11,6 @@ const menu = document.querySelectorAlls(".menu");
 // create a reference to the ".menu-button" class
 const menuButton = document.queryselectorAll(".menu-button");
 // Using your menuButton reference, add a click handler that calls toggleMenu
-menuButton.forEach(menuEl)=>{
-  menuOpen.addEventListener('click', )
+clickHandler() => {
+  menuOpen.addEventListener('click', toggleMenu() )
 }
